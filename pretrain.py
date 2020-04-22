c9bc1bc4d4641bd567d0ecbbd6a7e8bd9e4c7efa
MatyushinMA <Itsaprank@yandex.ru>
2020-04-22 12:23:24 +0300

    (FP) Full pretrain pipeline for handsfree pretraining

--- pretrain.py
+++ pretrain.py
@@ -7,12 +7,12 @@ import shutil
 
 class args:
     lr = 0.01
-    epochs = 100
-    eval_freq = 9500
-    print_freq = 5
+    epochs = 3
+    eval_freq = 13900
+    print_freq = 7000
     lmbd = 1.0
     ed_lmbd = 1.0
-    batch_size = 6
+    batch_size = 8
     noise_magnitude = 0.1
     pretrain_depth = 1
 
@@ -60,6 +60,13 @@ class AverageMeter(object):
         self.avg = self.sum / self.count
 
 model = get_model(num_classes=1, sample_size=224, width_mult=1.0, pretrain_depth=args.pretrain_depth)
+if args.pretrain_depth > 1:
+    try:
+        checkpoint = torch.load('./best_mobilenetv2_%d_checkpoint.pth.tar', map_location=torch.device('cpu'))['state_dict']
+        model.load_state_dict(checkpoint)
+        print('Loaded previous pretrain step weights')
+    except:
+        print('Training from scratch')
 model.classifier.eval()
 enc_parameters = 0
 dec_parameters = 0
@@ -69,61 +76,64 @@ for p in model.inv_features.parameters():
     dec_parameters += p.numel()
 number_of_parameters = enc_parameters + dec_parameters
 print('Encoder: %d, decoder: %d, total: %d' % (enc_parameters, dec_parameters, enc_parameters + dec_parameters))
-
 sometimes = lambda aug: iaa.Sometimes(0.8, aug)
 aug = iaa.Sequential([sometimes(iaa.AdditiveGaussianNoise(scale=args.noise_magnitude*255))])
 train_ds, test_ds = make_pretrain_datasets(aug=aug, batch_size=args.batch_size)
-optimizer = torch.optim.SGD(model.parameters(), lr=args.lr)
-loss_fn = torch.nn.MSELoss()
-best_acc = float('inf')
-for epoch in range(args.epochs):
-    losses = AverageMeter()
-    precs = AverageMeter()
-    ed_losses = AverageMeter()
-    grad_norms = AverageMeter()
-    model.cuda()
-    model.train()
-    train_ds.shuffle()
-    for i, (input, correct) in enumerate(train_ds):
+
+while model.pretrain_depth < len(model.features):
+    optimizer = torch.optim.SGD(model.parameters(), lr=args.lr)
+    loss_fn = torch.nn.MSELoss()
+    best_acc = float('inf')
+    for epoch in range(args.epochs):
+        losses = AverageMeter()
+        precs = AverageMeter()
+        ed_losses = AverageMeter()
+        grad_norms = AverageMeter()
+        model.cuda()
         model.train()
-        input, correct = input.cuda(), correct.cuda()
-        optimizer.zero_grad()
-        output = model.autoencode(input)
-        loss = loss_fn(output, correct)
-        loss_reg = torch.tensor(0.0).cuda()
-        for param in model.parameters():
-            if param.requires_grad:
-                loss_reg += (torch.norm(param))**2
-        loss_reg *= args.lmbd/(2*float(number_of_parameters))
-        loss += loss_reg
-        ed_loss = get_ed_reg_loss(model)*args.ed_lmbd
-        loss += ed_loss
-        ed_losses.update(ed_loss.item())
-        loss.backward()
-        with torch.no_grad():
-            grad_norm = 0
+        train_ds.shuffle()
+        for i, (input, correct) in enumerate(train_ds):
+            model.train()
+            input, correct = input.cuda(), correct.cuda()
+            optimizer.zero_grad()
+            output = model.autoencode(input)
+            loss = loss_fn(output, correct)
+            loss_reg = torch.tensor(0.0).cuda()
             for param in model.parameters():
                 if param.requires_grad:
-                    grad_norm += (torch.norm(param.grad)**2).item()
-        grad_norms.update(grad_norm)
-        losses.update(loss.item())
-        optimizer.step()
-        if i % args.eval_freq == 0 and i > 0:
+                    loss_reg += (torch.norm(param))**2
+            loss_reg *= args.lmbd/(2*float(number_of_parameters))
+            loss += loss_reg
+            ed_loss = get_ed_reg_loss(model)*args.ed_lmbd
+            loss += ed_loss
+            ed_losses.update(ed_loss.item())
+            loss.backward()
             with torch.no_grad():
-                model.eval()
-                for j, (einput, ecorrect) in enumerate(test_ds):
-                    einput, ecorrect = einput.cuda(), ecorrect.cuda()
-                    eoutput = model.autoencode(einput)
-                    prec = loss_fn(eoutput, ecorrect)
-                    precs.update(prec)
-        if i % args.print_freq == 0 and i > 0:
-            print('Epoch: %d(%d/%d)\tLoss: %f\tED_loss: %f\tPrec: %f\tGrad: %f\n%s' % (epoch, i, len(train_ds), losses.avg, ed_losses.avg, precs.avg, grad_norms.avg, '-'*80))
-    save_checkpoint({
-            'epoch' : epoch + 1,
-            'state_dict' : model.state_dict(),
-            'prec' : precs.avg,
-        }, precs.avg < best_acc, precs.avg, filename='mobilenetv2_%d_checkpoint.pth' % args.pretrain_depth)
+                grad_norm = 0
+                for param in model.parameters():
+                    if param.requires_grad:
+                        grad_norm += (torch.norm(param.grad)**2).item()
+            grad_norms.update(grad_norm)
+            losses.update(loss.item())
+            optimizer.step()
+            if i % args.eval_freq == 0 and i > 0:
+                with torch.no_grad():
+                    model.eval()
+                    for j, (einput, ecorrect) in enumerate(test_ds):
+                        einput, ecorrect = einput.cuda(), ecorrect.cuda()
+                        eoutput = model.autoencode(einput)
+                        prec = loss_fn(eoutput, ecorrect)
+                        precs.update(prec)
+            if i % args.print_freq == 0 and i > 0:
+                print('Depth: %d\tEpoch: %d(%d/%d)\tLoss: %f\tED_loss: %f\tPrec: %f\tGrad: %f\n%s' % (args.pretrtain_depth, epoch, i, len(train_ds), losses.avg, ed_losses.avg, precs.avg, grad_norms.avg, '-'*80))
+        save_checkpoint({
+                'epoch' : epoch + 1,
+                'state_dict' : model.state_dict(),
+                'prec' : precs.avg,
+            }, precs.avg < best_acc, precs.avg, filename='mobilenetv2_%d_checkpoint.pth' % args.pretrain_depth)
 
-    best_acc = min(precs.avg, best_acc)
-    adjust_learning_rate(optimizer, epoch, args.lr)
-os.system('rm mobilenetv2_%s_checkpoint.pth.tar' % args.pretrain_depth)
+        best_acc = min(precs.avg, best_acc)
+        adjust_learning_rate(optimizer, epoch, args.lr)
+    os.system('rm mobilenetv2_%s_checkpoint.pth.tar' % args.pretrain_depth)
+    args.pretrain_depth += 1
+    model.pretrain_depth = args.pretrain_depth
